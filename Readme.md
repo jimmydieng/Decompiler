ba64cde4128e540bbf2ef271301ef1ce8ba70479
Jimmy <jimmydieng@gmail.com>
2016-07-26 12:31:20 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Credits
+
+Props to:
+https://gist.github.com/mattak/5337300
+for the start.sh script
---
+++ start.sh
@@ -0,0 +1,48 @@
+    #!/bin/sh
+
+if [ $# -lt 1 ]; then
+  echo "usage: [apk file]"
+  exit 0
+fi
+
+APKFILE=$1
+APKTOOL=`pwd`/apktool-cli.jar
+D2J=`pwd`/dex2jar-2.0/d2j-dex2jar.sh
+JAD=`pwd`/jad
+OUTPUTDIR=`echo $APKFILE | sed 's/\.apk//g'`
+TMPDIR=tmp
+
+#
+# 1. apk -> AndroidManifest.xml smali res
+#
+echo java -jar $APKTOOL d $APKFILE $OUTPUTDIR
+java -jar $APKTOOL d $APKFILE $OUTPUTDIR
+
+#
+# 2. apk -> zip
+#
+unzip $APKFILE -d $TMPDIR
+
+# 
+# 3. classes.jar -> classes/
+# 
+mkdir $TMPDIR
+mkdir $TMPDIR/classes
+$D2J $TMPDIR/classes.dex --output $TMPDIR/classes/classes.jar
+
+cd $TMPDIR/classes
+jar xvf classes.jar
+
+# 
+# 4. classes/ -> src/
+# 
+$JAD -s java -d ../src -r ./**/*.class
+cd -
+
+mv $TMPDIR/src $OUTPUTDIR/
+
+#
+# 5. remove tmp dir
+# 
+rm -rf $TMPDIR
+
